4e1c942ba50c42775d56c6d88a14710b87662eee
Dorothea Hinrichs <dorothea.hinrichs@posteo.net>
2022-08-10 19:24:21 +0100

    all done apart from that last extension

--- models/traveller.js
+++ models/traveller.js
@@ -3,28 +3,50 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const result = []
+  this.journeys.forEach((journey) => {
+    result.push(journey.startLocation)
+  });
+  return result
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const results = []
+  this.journeys.forEach((journey) => {
+    results.push(journey.endLocation)
+  });
+  return results
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(journey => {
+    return journey.transport === transport
+  });
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter(journey => {
+    return journey.distance > minDistance
+  });
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  let total = this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal += journey.distance;
+  }, 0);
+  return total
 };
 
-Traveller.prototype.getUniqueModesOfTransport = function () {
 
+Traveller.prototype.getUniqueModeOfTransport = function(){ 
 };
+
+
+// Traveller.prototype.getUniqueModesOfTransport = function (){
+//   return this.journeys.map(journey => {
+//     return journey.transport;
+//   });
+// };
 
 
 module.exports = Traveller;
